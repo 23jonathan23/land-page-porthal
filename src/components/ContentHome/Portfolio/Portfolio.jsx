9720d117a73e7ec3d7ba7680aa7d7ff9c54a4fad
Jonathan Raphael <jonathanraphael74@outlook.com>
2020-05-21 20:48:20 -0300

    Portfolio Updated Complete

--- src/components/ContentHome/Portfolio/Portfolio.jsx
+++ src/components/ContentHome/Portfolio/Portfolio.jsx
@@ -4,49 +4,10 @@ import './Portfolio.css'
 
 import PortfolioItem from './PortfolioItem/PortfolioItem'
 
-export default props => {
-  const publication = [
-    {
-      id: 1,
-      title: 'Biclicle',
-      subTitle: 'New bicicle and new price',
-      imageLink: 'http://forbetterweb.com/html/pheromone/img/portfolio/7.jpg',
-      content: 'Uma bicicleta fantastica e sensacional',
-    },
-    {
-      id: 2,
-      title: 'Apple',
-      subTitle: 'New Apple and new price',
-      imageLink:
-        'https://cdn.pixabay.com/photo/2020/05/17/19/43/apple-5183288_960_720.jpg',
-      content: 'Uma Maça fantastica e sensacional',
-    },
-    {
-      id: 3,
-      title: 'Jeans',
-      subTitle: 'New Jeans and new price',
-      imageLink:
-        'https://cdn.pixabay.com/photo/2014/08/26/21/49/jeans-428614_960_720.jpg',
-      content: 'Uma Jeans fantastica e sensacional',
-    },
-    {
-      id: 4,
-      title: 'Wacth',
-      subTitle: 'New Wacth and new price',
-      imageLink:
-        'https://cdn.pixabay.com/photo/2014/07/31/23/00/wristwatch-407096_960_720.jpg',
-      content: 'Uma Relógio fantastico e sensacional',
-    },
-    {
-      id: 5,
-      title: 'Wacth',
-      subTitle: 'New Wacth and new price',
-      imageLink:
-        'https://cdn.pixabay.com/photo/2014/07/31/23/00/wristwatch-407096_960_720.jpg',
-      content: 'Uma Relógio fantastico e sensacional',
-    },
-  ]
+//Lista de postagens teste
+import { publication } from './list'
 
+export default props => {
   const renderPortfolioItem = p => {
     return p.map(i => <PortfolioItem key={i.id} publication={i} />)
   }
---
+++ src/components/ContentHome/Portfolio/list.js
@@ -0,0 +1,129 @@
+export const publication = [
+  {
+    id: 1,
+    title: 'Bicicleta',
+    subTitle: 'Descrição do subtitle',
+    imageLink: 'http://forbetterweb.com/html/pheromone/img/portfolio/7.jpg',
+    content: 'fantastica e sensacional',
+  },
+  {
+    id: 2,
+    title: 'Martelo',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2017/08/10/03/46/hammer-2617863_960_720.jpg',
+    content: 'fantastica e sensacional',
+  },
+  {
+    id: 3,
+    title: 'Câmera',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2014/02/22/19/56/display-272270_960_720.jpg',
+    content: 'fantastica e sensacional',
+  },
+  {
+    id: 4,
+    title: 'Canela',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2015/03/26/10/21/cinnamon-sticks-691190_960_720.jpg',
+    content: 'fantastico e sensacional',
+  },
+  {
+    id: 5,
+    title: 'Ponte',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2019/10/13/13/28/bridge-4546141_960_720.jpg',
+    content: 'fantastico e sensacional',
+  },
+  {
+    id: 6,
+    title: 'Objetos e plantas',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2017/02/08/02/59/objects-2047602_960_720.jpg',
+    content: 'fantastico e sensacional',
+  },
+  {
+    id: 7,
+    title: 'Celular',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2015/01/16/15/01/smartphone-601554_960_720.jpg',
+    content: 'fantastico e sensacional',
+  },
+  {
+    id: 8,
+    title: 'Relógio',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2014/04/05/11/40/clock-316604_960_720.jpg',
+    content: 'fantastico e sensacional',
+  },
+  {
+    id: 9,
+    title: 'Óculos',
+    subTitle: 'NeDescrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2017/08/10/07/15/eyeglasses-2619331_960_720.jpg',
+    content: 'Uma bfantastica e sensacional',
+  },
+  {
+    id: 10,
+    title: 'Fone',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2014/10/07/09/17/earphones-477446_960_720.jpg',
+    content: 'fantastica e sensacional',
+  },
+  {
+    id: 11,
+    title: 'Caixa de som',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2015/07/11/22/58/object-841416_960_720.jpg',
+    content: 'fantastica e sensacional',
+  },
+  {
+    id: 12,
+    title: 'Câmera 2',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2014/02/22/19/54/display-272269_960_720.jpg',
+    content: 'fantastico e sensacional',
+  },
+  {
+    id: 13,
+    title: 'Quadro',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2020/05/07/03/35/delegate-5140018_960_720.jpg',
+    content: 'fantastico e sensacional',
+  },
+  {
+    id: 14,
+    title: 'Caixa de som 2',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2017/08/10/04/07/bose-2618027_960_720.jpg',
+    content: 'fantastico e sensacional',
+  },
+  {
+    id: 15,
+    title: 'Carro',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2015/06/01/14/58/machine-793633_960_720.jpg',
+    content: 'fantastico e sensacional',
+  },
+  {
+    id: 16,
+    title: 'Origami',
+    subTitle: 'Descrição do subtitle',
+    imageLink:
+      'https://cdn.pixabay.com/photo/2013/11/13/15/02/origami-210043_960_720.jpg',
+    content: 'fantastico e sensacional',
+  },
+]
